d627168a64760126b94cc37a1c6d577da7406917
Guilherme Henrique Mota Alfredo <guilhermehmota12@gmail.com>
2021-07-05 16:33:29 -0300

    correções no login e signup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { userRouter } from "./routes/UserRouter";
+import {AddressInfo} from "net";
 
 
 const app = express();
@@ -7,3 +8,13 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", userRouter);
+
+
+const server = app.listen(3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Servidor rodando em http://localhost:${address.port}`);
+    } else {
+        console.error(`Falha ao rodar o servidor`);
+    }
+});
