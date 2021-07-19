c510090f7d2b5c6d871248c02a6fa801c18ffd59
Guilherme Henrique Mota Alfredo <guilhermehmota12@gmail.com>
2021-07-19 15:09:16 -0300

    erros port

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ app.use("/images", imageRouter);
 
 
 
-const PORT = process.env.PORT || 3003;
-const server = app.listen(PORT, () => {
+const PORT = process.env.APP_PORT || 3004;
+const server = app.listen(Number(PORT), () => {
     if (server) {
         const address = server.address() as AddressInfo;
         console.log(`Servidor rodando em http://localhost:${address.port}`);
